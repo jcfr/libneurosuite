8bfd296041d16b19008f3313c7dd52f0d508d672
Montel Laurent <montel@kde.org>
2012-09-03 19:53:29 +0200

    Add qpagedialogwidget

---
+++ src/qpagedialogwidget.cpp
@@ -0,0 +1,10 @@
+/*
+Copyright (C) 2012 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+*/
+
+
+#include "qpagedialogwidget.h"
+
+QPageDialogWidget::QPageDialogWidget()
+{
+}
---
+++ src/qpagedialogwidget.h
@@ -0,0 +1,14 @@
+/*
+Copyright (C) 2012 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+*/
+
+#ifndef QPAGEDIALOGWIDGET_H
+#define QPAGEDIALOGWIDGET_H
+
+class QPageDialogWidget
+{
+public:
+    QPageDialogWidget();
+};
+
+#endif // QPAGEDIALOGWIDGET_H
