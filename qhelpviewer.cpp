be77c0fa2427e082ac1f8a6938f4822c87d7aea0
Montel Laurent <montel@kde.org>
2012-09-12 16:54:02 +0200

    Start to create a qhelpviewer for doc

---
+++ qhelpviewer.cpp
@@ -0,0 +1,14 @@
+/*
+Copyright (C) 2012 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+*/
+
+#include "qhelpviewer.h"
+
+QHelpViewer::QHelpViewer(QWidget *parent)
+    :QDialog(parent)
+{
+}
+
+QHelpViewer::~QHelpViewer()
+{
+}
---
+++ qhelpviewer.h
@@ -0,0 +1,15 @@
+/*
+Copyright (C) 2012 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+*/
+
+#ifndef QHELPVIEWER_H
+#define QHELPVIEWER_H
+
+class QHelpViewer : public QDialog
+{
+public:
+    explicit QHelpViewer(QWidget *parent);
+    ~QHelpViewer();
+};
+
+#endif // QHELPVIEWER_H
