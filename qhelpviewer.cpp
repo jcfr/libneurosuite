ff2ccdf054056c80a010350c5a643d939d4c3661
Montel Laurent <montel@kde.org>
2012-09-12 18:06:31 +0200

    Improve++

--- qhelpviewer.cpp
+++ qhelpviewer.cpp
@@ -3,12 +3,20 @@ Copyright (C) 2012 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kda
 */
 
 #include "qhelpviewer.h"
+#include <QHBoxLayout>
 
 QHelpViewer::QHelpViewer(QWidget *parent)
     :QDialog(parent)
 {
+    QHBoxLayout *lay = new QHBoxLayout;
+    setLayout(lay);
 }
 
 QHelpViewer::~QHelpViewer()
 {
+}
+
+void QHelpViewer::setHtml(const QString& filename)
+{
+    //TODO
 }
--- qhelpviewer.h
+++ qhelpviewer.h
@@ -4,12 +4,15 @@ Copyright (C) 2012 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kda
 
 #ifndef QHELPVIEWER_H
 #define QHELPVIEWER_H
+#include <QDialog>
 
 class QHelpViewer : public QDialog
 {
 public:
     explicit QHelpViewer(QWidget *parent);
     ~QHelpViewer();
+    void setHtml(const QString& filename);
+private:
 };
 
 #endif // QHELPVIEWER_H
