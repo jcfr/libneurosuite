b0350f5964f5a7a7d047790a6520c29ffc101653
Michaël Zugaro <michael.zugaro@college-de-france.fr>
2013-08-19 16:08:18 +0200

    GPL v3

--- src/shared/zoomwindow.cpp
+++ src/shared/zoomwindow.cpp
@@ -2,7 +2,7 @@
                           ZoomWindow.cpp  -  description
                              -------------------
     begin                : Thu Aug 21 2003
-    copyright            : (C) 2003 by
+    copyright            : (C) 2003 by Lynn Hazan
     email                :
 
  ***************************************************************************/
@@ -11,7 +11,7 @@
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
+ *   the Free Software Foundation; either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
