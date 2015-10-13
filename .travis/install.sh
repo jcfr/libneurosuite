3332a3463a0d336e09f9912f7ed964c966d0daf2
Florian Franzen <FlorianFranzen@gmail.com>
2015-10-13 18:23:07 +0200

    Fix cmake install on linux and qt5 linking on OS X

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,7 +6,7 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     fi
     sudo add-apt-repository -y ppa:smspillaz/cmake-2.8.12
     sudo apt-get update
-    sudo apt-get install -y cmake
+    sudo apt-get install -y cmake cmake-data
 
     if [ "$QT_VERSION" = "qt4" ]; then
         sudo apt-get install -y libqtgui4
@@ -19,8 +19,10 @@ fi
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
     brew update
-    brew install cmake $QT_VERSION
+    brew install $QT_VERSION
     if [ "$QT_VERSION" = "qt5" ]; then
         brew link --force qt5
+        ln -s /usr/local/opt/qt5/mkspecs /usr/local/mkspecs
+        ln -s /usr/local/opt/qt5/plugins /usr/local/plugins
     fi
 fi
